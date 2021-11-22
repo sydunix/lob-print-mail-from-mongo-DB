1e6635fbf6ad46726ff8999c648d61daf8c5b9e2
sjekpo <sydney.j.ekpo@gmail.com>
2021-11-22 11:16:13 +0100

    added sampleData to entryList file

---
+++ entryList.js
@@ -0,0 +1,83 @@
+
+export const sampleData = [
+    {
+        name: "Kelly Jones",
+        amount: 185.54,
+        street: "185 Berry St,",
+        apartment: "",
+        city: "San Francisco",
+        state: "CA",
+        zip_code: 94107,
+        country: "US"
+    },
+
+    {
+        name: "Margaret Smith",
+        amount: 387.45,
+        street: "6575 W Rialto",
+        apartment: "",
+        city: "fresno",
+        state: "CA",
+        zip_code: 93723,
+        country: "US"
+    },
+
+    {
+        name: "Jess Smith",
+        amount: 19.87,
+        street: "537 Fillmore Street",
+        apartment: "Apt #2",
+        city: "San Francicso",
+        state: "CA",
+        zip_code: 94117,
+        country: "US"
+    },
+
+    {
+        name: "Michael Thruman",
+        amount: 347.21,
+        street: "1625 Post St",
+        apartment: "",
+        city: "San Francicso",
+        state: "CA",
+        zip_code: 94115,
+        country: "US"
+    },
+
+    {
+        name: "Gerald Merritt",
+        amount: 278.4,
+        street: "333 Post St",
+        apartment: "",
+        city: "San Francicso",
+        state: "CA",
+        zip_code: 94108,
+        country: "US"
+    },
+
+    {
+        name: "Linda Anderson",
+        amount: 425.4,
+        street: "655 Fake Address",
+        apartment: "Fake Apartment",
+        city: "Not a City",
+        state: "CA",
+        zip_code: 06475-1246,
+        country: "US"
+
+    },
+
+    {
+        name: "John Travolta",
+        amount: 175.84,
+        street: "111 NotAnAddress",
+        apartment: "",
+        city: "San Francisco",
+        state: "CA",
+        zip_code: 94107,
+        country: "US"
+    }
+
+    ];
+
+module.exports = entryList;
